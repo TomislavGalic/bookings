72401b61498418a945a5b747212a74830ac0cd6b
TomislavGalic <tomislav.galic19@gmail.com>
2022-10-07 21:42:34 +0200

    commit; templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,44 @@
 package handlers
 
 import (
-	""
 	"net/http"
+
+	"github.com/TomislavGalic/bookings/pkg/config"
+	"github.com/TomislavGalic/bookings/pkg/models"
+	"github.com/TomislavGalic/bookings/pkg/render"
 )
 
+// Repository used by the handlers
+var Repo *Repository
+
+// Repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// Creates the new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// Sets the repository for the handlers
+func NewHandlers(r *Repository) {
+
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
